ef7f552050d422f8ea706993e6431e542a080f0d
Lars Strojny <lars@strojny.net>
2012-11-08 21:29:04 +0100

    Sort files in test report

--- test-report.sh
+++ test-report.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-find tests -type f -and -name "*.diff" -or -name "*.out" | while read file; do
+find tests -type f -and -name "*.diff" -or -name "*.out" | sort | while read file; do
     echo "FILE: ${file}"
     cat "$file"
     echo "\n"
