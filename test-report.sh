f440f6871f23ede3a55744dc016fb5c8c16aebf7
Lars Strojny <lars@strojny.net>
2012-11-11 19:01:52 +0100

    Didn’t know: shell starts a subshell for "while read foo" expression

--- test-report.sh
+++ test-report.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-fail=0
-find tests -type f -and -name "*.diff" -or -name "*.out" | sort | while read file; do
+result=`find tests -type f -and -name "*.diff" -or -name "*.out" | sort | while read file; do
     echo "FILE: ${file}"
     cat "$file"
     echo "\n"
-    fail=1
-done
+done`
 
-test $fail -gt 0 && exit 1
+if [ -n "$result" ]; then
+    echo "$result"
+    exit 1
+fi
