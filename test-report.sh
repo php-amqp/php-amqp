52f753fb1a81831ffa4f64ff38d9842ce92dc5a0
Lars Strojny <lars@strojny.net>
2012-11-11 18:50:18 +0100

    Move return code evaluation to test-report.sh

--- test-report.sh
+++ test-report.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+fail=0
 find tests -type f -and -name "*.diff" -or -name "*.out" | sort | while read file; do
     echo "FILE: ${file}"
     cat "$file"
     echo "\n"
+    fail=1
 done
+
+$fail && exit 1
