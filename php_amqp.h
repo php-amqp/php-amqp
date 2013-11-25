2ede096c5be8a9202dda5009641cc998c5f20ada
Lars Strojny <lars@strojny.net>
2013-11-25 10:59:35 +0100

    Release management

--- php_amqp.h
+++ php_amqp.h
@@ -409,7 +409,7 @@ typedef struct _amqp_envelope_object {
 #define AMQP_G(v) (amqp_globals.v)
 #endif
 
-#define PHP_AMQP_VERSION "1.2.1-dev"
+#define PHP_AMQP_VERSION "1.3.0"
 
 #endif	/* PHP_AMQP_H */
 
