5d4fede0cc4855129f170c76142e3b93159681bd
Lars Strojny <lars@strojny.net>
2015-01-27 15:15:43 +0100

    #140: pull 1.6.0beta1, push beta2

--- php_amqp.h
+++ php_amqp.h
@@ -424,7 +424,7 @@ typedef struct _amqp_envelope_object {
 #endif
 
 #ifndef PHP_AMQP_VERSION
-#define PHP_AMQP_VERSION "1.6.0beta1"
+#define PHP_AMQP_VERSION "1.6.0beta2"
 #endif
 
 #ifndef PHP_AMQP_REVISION
