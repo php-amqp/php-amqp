f392863ae06d586975329c1ee1a8e4007d976071
Lars Strojny <lars@strojny.net>
2015-09-14 14:39:46 +0200

    RM

--- php_amqp.h
+++ php_amqp.h
@@ -439,7 +439,7 @@ typedef struct _amqp_envelope_object {
 #endif
 
 #ifndef PHP_AMQP_VERSION
-#define PHP_AMQP_VERSION "1.6.0beta3"
+#define PHP_AMQP_VERSION "1.6.0beta4"
 #endif
 
 #ifndef PHP_AMQP_REVISION
