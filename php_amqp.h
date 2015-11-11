e326b8f60867099cc7b9e1e0a84c5174ee046028
Lars Strojny <lars@strojny.net>
2015-11-11 18:01:37 +0100

    RM 1.7.0alpha1

--- php_amqp.h
+++ php_amqp.h
@@ -320,7 +320,7 @@ typedef struct _amqp_connection_object {
 #endif
 
 #ifndef PHP_AMQP_VERSION
-#define PHP_AMQP_VERSION "1.6.0"
+#define PHP_AMQP_VERSION "1.7.0alpha1"
 #endif
 
 #ifndef PHP_AMQP_REVISION
