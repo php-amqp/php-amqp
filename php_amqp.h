c3b67ba85e5bd1f0e1191e54362b657c009447e0
Lars Strojny <lars.strojny@internations.org>
2020-04-05 16:08:45 +0200

    [RM] back to dev 1.10.2dev

--- php_amqp.h
+++ php_amqp.h
@@ -368,7 +368,7 @@ ZEND_EXTERN_MODULE_GLOBALS(amqp);
 #endif
 
 #ifndef PHP_AMQP_VERSION
-#define PHP_AMQP_VERSION "1.10.1"
+#define PHP_AMQP_VERSION "1.10.2dev"
 #endif
 
 #ifndef PHP_AMQP_REVISION
