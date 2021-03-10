87d2ca94a3536b7618aa1c865d56278b17d127f9
Lars Strojny <lars.strojny@internations.org>
2021-03-10 09:55:38 +0100

    [RM] releasing version 1.11.0beta

--- php_amqp.h
+++ php_amqp.h
@@ -368,7 +368,7 @@ ZEND_EXTERN_MODULE_GLOBALS(amqp);
 #endif
 
 #ifndef PHP_AMQP_VERSION
-#define PHP_AMQP_VERSION "1.10.3dev"
+#define PHP_AMQP_VERSION "1.11.0beta"
 #endif
 
 #ifndef PHP_AMQP_REVISION
